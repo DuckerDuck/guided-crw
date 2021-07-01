4ed518e45208bb63055a81b518ef4f5636f6dc97
Jan Schutte <jan.o.schutte@gmail.com>
2021-07-01 16:42:21 +0200

    JHMDB: Only run test on validation set only Before this commit the JHMDB validation was ran on both train + validation sets, while that is fine in an unsupervised setting it is not the same as done in other papers.

--- code/eval/gen_jhmbd.py
+++ code/eval/gen_jhmbd.py
@@ -6,15 +6,35 @@ with open('jhmdb_imglist.txt') as f:
 with open('jhmdb_lbllist.txt') as f:
     lbllist = f.readlines()
 
+with open('jhmdb_split1.txt') as f:
+    splitlist = f.readlines()
+
 imglist = [img.strip() for img in imglist]
 lbllist = [lbl.strip() for lbl in lbllist]
 
+val_split = []
+for vid_split in splitlist:
+    vid = vid_split.strip()
+    vid, is_val = vid.split(' ')
+    if is_val == '2':
+        val_split.append(vid.replace('.avi', ''))
+
+
 for label in lbllist:
     components = label.split('/')
-    iden = components[5]
+    iden = components[6]
+
+    # Check iden in validation set
+    found = False
+    for val in val_split:
+        if val == iden:
+            found = True
+            break
+
+    if not found:
+        continue
     
     for img in imglist:
         img_components = img.split('/')
-        if iden == img_components[5]:
+        if iden == img_components[6]:
             print(img, label)
-
--- code/gen_jhmdb_lists.sh
+++ code/gen_jhmdb_lists.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 
-find ../../JHMDB/Rename_Images/ -mindepth 2 -type d | grep -v .AppleDouble > eval/jhmdb_imglist.txt
-find ../../JHMDB/joint_positions | grep -v .AppleDouble | grep mat > eval/jhmdb_lbllist.txt
+find ../../datasets/jhmbd/Rename_Images/ -mindepth 2 -type d | grep -v .AppleDouble > eval/jhmdb_imglist.txt
+find ../../datasets/jhmbd/joint_positions | grep -v .AppleDouble | grep mat > eval/jhmdb_lbllist.txt
 
 cd eval
 python3 gen_jhmbd.py > jhmdb_vallist.txt
