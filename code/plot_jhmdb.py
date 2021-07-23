e2c1c6125a5b6393dc643ea98296f8e1676c848e
Jan Schutte <jan.o.schutte@gmail.com>
2021-07-23 09:18:41 +0200

    PLot jhmdb based on vallist

--- code/plot_jhmdb.py
+++ code/plot_jhmdb.py
@@ -6,38 +6,63 @@ from data.jhmdb import load_image
 
 from PIL import Image
 
+def read_vallist(args):
+
+    with open(args.vallist) as f:
+        lines = f.readlines()
+
+    result = []
+    for line in lines:
+        line = line.strip()
+        imgs, vids = line.split(' ')
+        result.append((imgs, vids))
+    return result
+
+
 def main(args):
-    predres  = np.load(args.joints, allow_pickle=True)
-    loc, joints, frames = predres.shape
-
     map_scale = np.array([8, 8])
-    print(map_scale)
 
-    img_base = Path(args.background)
-    if not img_base.exists():
-        print(f'Background folder {img_base} not found!')
-        return
+    vids = read_vallist(args)
+    output_dir = Path(args.output)
+    if not output_dir.exists():
+        output_dir.mkdir()
 
-    for f in range(frames):
-        img_path = img_base / (str(f+1).zfill(5) + '.png')
-        if not img_path.exists():
-            print(f'Could not find image {img_path}!')
+    for i, (imgs, lbls) in enumerate(vids):
+        print(i, imgs)
+        joint_file = Path(args.joints) / (str(i) + '.dat')
+        video_dir = output_dir / str(i)
+
+        if not video_dir.exists():
+            video_dir.mkdir()
+
+        predres  = np.load(joint_file, allow_pickle=True)
+        loc, joints, frames = predres.shape
+
+        img_base = Path(imgs)
+        if not img_base.exists():
+            print(f'Background folder {img_base} not found!')
             return
-        
-        coords = predres[:, :, f].squeeze()
-        img = load_image(str(img_path)).permute(1,2,0).numpy() * 255
-        pose_map = vis_pose(img, coords * map_scale[..., None])
 
-        pose_map = pose_map.astype(np.uint8)
+        for f in range(frames):
+            img_path = img_base / (str(f+1).zfill(5) + '.png')
+            if not img_path.exists():
+                print(f'Could not find image {img_path}!')
+                return
+            
+            coords = predres[:, :, f].squeeze()
+            img = load_image(str(img_path)).permute(1,2,0).numpy() * 255
+            pose_map = vis_pose(img, coords * map_scale[..., None])
 
-        with open(args.output + str(f+1).zfill(5) + '.jpg', 'w') as file_ptr:
-            img = Image.fromarray(pose_map)
-            img.save(file_ptr, format='jpeg', quality=90)
+            pose_map = pose_map.astype(np.uint8)
+            output_file = video_dir / (str(f+1).zfill(5) + '.jpg')
+            with open(output_file, 'w') as file_ptr:
+                img = Image.fromarray(pose_map)
+                img.save(file_ptr, format='jpeg', quality=90)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Plot Skeletons of JHMDB results')
     parser.add_argument('joints', help='Path to .dat file containing joint coordinates')
-    parser.add_argument('background', help='Path to jpg file with background')
+    parser.add_argument('vallist', help='JHMDB vallist')
     parser.add_argument('--output', default='./JHMDB_visualized/', help='Path to plot folder')
     
     args = parser.parse_args()
